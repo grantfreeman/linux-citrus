7e02cc08c95d54c3ad4b3c75c07f2d95e4fd16cd
grant freeman <grantfreeman@pm.me>
2023-05-05 00:40:28 -0500

    ankama cinematic removal for dofus

---
+++ ankama/remove-cinematics.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# class preview cinematics cause the client to crash.
+# needs to be run before making new characters.
+rm /home/$USER/.config/Ankama/Dofus/content/gfx/cinematics/class*
